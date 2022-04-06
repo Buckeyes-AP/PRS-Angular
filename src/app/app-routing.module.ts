2954fca75ae44c5513761c82db4858b3d51d46e7
Alex Poirier <alexmatthewpoirier@gmail.com>
2022-04-06 16:26:32 -0400

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { E404Component } from './e404/e404.component';
+import { HomeComponent } from './home/home.component';
+import { UserCreateComponent } from './prs/user/user-create/user-create.component';
+import { UserDetailComponent } from './prs/user/user-detail/user-detail.component';
+import { UserEditComponent } from './prs/user/user-edit/user-edit.component';
+import { UserListComponent } from './prs/user/user-list/user-list.component';
+import { VendorCreateComponent } from './prs/vendor/vendor-create/vendor-create.component';
+import { VendorDetailComponent } from './prs/vendor/vendor-detail/vendor-detail.component';
+import { VendorEditComponent } from './prs/vendor/vendor-edit/vendor-edit.component';
+import { VendorListComponent } from './prs/vendor/vendor-list/vendor-list.component';
+import { Vendor } from './prs/vendor/vendor.class';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+
+  {path: "user/list", component: UserListComponent},
+  {path: "user/create", component: UserCreateComponent},
+  {path: "user/detail/:id", component: UserDetailComponent},
+  {path: "user/edit/:id", component: UserEditComponent},
+
+  {path: "vendor/list", component: VendorListComponent},
+  {path: "vendor/create", component: VendorCreateComponent},
+  {path: "vendor/detail/:id", component: VendorDetailComponent},
+  {path: "vendor/edit/:id", component: VendorEditComponent},
+
+
+  {path: "**", component: E404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,47 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { E404Component } from './e404/e404.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { UserListComponent } from './prs/user/user-list/user-list.component';
+import { UserDetailComponent } from './prs/user/user-detail/user-detail.component';
+import { UserCreateComponent } from './prs/user/user-create/user-create.component';
+import { UserEditComponent } from './prs/user/user-edit/user-edit.component';
+import { VendorListComponent } from './prs/vendor/vendor-list/vendor-list.component';
+import { VendorEditComponent } from './prs/vendor/vendor-edit/vendor-edit.component';
+import { VendorDetailComponent } from './prs/vendor/vendor-detail/vendor-detail.component';
+import { VendorCreateComponent } from './prs/vendor/vendor-create/vendor-create.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    E404Component,
+    HomeComponent,
+    MenuComponent,
+    UserListComponent,
+    UserDetailComponent,
+    UserCreateComponent,
+    UserEditComponent,
+    VendorListComponent,
+    VendorEditComponent,
+    VendorDetailComponent,
+    VendorCreateComponent,
+
+   
+    
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule, HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
